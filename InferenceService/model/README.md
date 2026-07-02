e0bc0dacf462a06e6de2aecd0563f0b7a610cd90
TomasConti02 <tomasconti34@gmail.com>
2026-07-02 15:54:58 +0200

    add trasnformer with kafka network call system

--- InferenceService/model/README.md
+++ InferenceService/model/README.md
@@ -25,3 +25,17 @@ docker push <your docker hub user>/image-transformer:vx
 -----------------------------------------------------------------------
 
 While multi-stage builds are effective at excluding build-time toolchains, the final image remains significant in size. This is due to the inherent weight of the TensorFlow library, which is a required dependency for the inference and transformation logic. Even in its optimized form, the runtime footprint includes the necessary core libraries to ensure model compatibility
+
+----
+
+### Kserve Inference model
+
+KServe is an enterprise MLOps framework designed to deploy machine learning models as highly scalable microservices on Kubernetes. To maximize resource efficiency, KServe decouples data processing from heavy tensor computation by separating the workload into distinct components: a Transformer (handling pre- and post-processing) and a Predictor (dedicated solely to model inference).
+
+Inside the Transformer container, a dual-engine processing model utilizes two distinct execution environments running side-by-side to guarantee low-latency request handling:
+
+- Synchronous Engine (Main Request Threads): Managed natively by KServe's web server. Its primary responsibility is high-speed execution of data transformation algorithms (preprocess and postprocess) and making synchronous HTTP/gRPC routing calls directly to the model Predictor.
+
+- Asynchronous Engine (Background Worker Thread): A dedicated, non-blocking background companion driven by a custom Python asyncio event loop. Its isolated priority is managing concurrent network ingestion and streaming event payloads out to the Knative/Kafka broker via CloudEvents, completely out of the critical path of the user request.
+
+The main idea is to avoid Main Thread blocked because network call latency.
--- InferenceService/model/transformer_final.py
+++ InferenceService/model/transformer_final.py
@@ -1,32 +1,89 @@
 import argparse
 import base64
 import logging
-import os
-import numpy as np
+import threading
+import asyncio
+import uuid
+import aiohttp
 import tensorflow as tf
+
 from kserve import Model, ModelServer
 from typing import Any, Dict, List, Optional
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
-"""
-Kserve have a main CRD InferenceService composed by :
-1. Predicotor, executor of inference model 
-2. Transformer of the model that execute pre processing and post processing
-"""
+
+# Custom KServe Transformer, it execute a pre and post processing o images and execute the model predictions
+# asynchronously dispatches the prediction embeddings to a Knative / Kafka Eventing Broker by CloudEvents
 class ImageTransformer(Model):
-    def __init__(self, name: str, namespace: str, predictor_port: int = 8080):
+    def __init__(self, name: str, namespace: str, predictor_port: int = 8080,
+                 broker: str = "http://kafka-broker-ingress.knative-eventing.svc.cluster.local/default/kafka-broker",
+                 broker_host: str = "kafka-broker-ingress.knative-eventing.svc.cluster.local",
+                 ce_type: str = "org.kubeflow.serving.inference.request"):
+        
         super().__init__(name)
         self.ready = True
+        self.count = 0
 
-        #Transformer have to know the predictor logic endpoint 
+        self.broker = broker
+        self.broker_host = broker_host
+        self.ce_type = ce_type
+
         self.predictor_port = predictor_port
-        self.predictor_host = f"{name}-predictor.{namespace}.svc.cluster.local" # dynamic inference service resolution acoording to Knative URL policy
-        logger.info(f"==> Model [{name}] conf and Predictor Host: {self.predictor_host}:{self.predictor_port}")
+        self.predictor_host = f"{name}-predictor.{namespace}.svc.cluster.local"
 
-    def preprocess(  self, payload: Dict[str, Any], headers: Optional[Dict[str, str]] = None ) -> Dict[str, List[List[float]]]:
-        #pre processing example of task, receive the @json image into the payload and coded into base64
-        #  decode the  b64, conver in gray scale, resize int 28X28 and normalize before sending to model 
+        # Sending data to Kafka over HTTP can introduce network latency, 
+        # doing it directly inside postprocess would slow down user response times
+        # The code uses a dedicated background working thread to handle Kafka side-effects
+        #sets a multi-threaded asynchronous 
+        self.loop = asyncio.new_event_loop() #create a event loop engine
+        #remember in prodaction fix a size of queue to avoid memory issue
+        self.event_queue = asyncio.Queue(loop=self.loop) #now the loop engine is attach to a FIFO queue
+        self.worker_thread = threading.Thread(target=self._run_worker, daemon=True) #back ground and asynch thread that handle network
+        self.worker_thread.start()
+
+        logger.info(f"==> Model [{name}] Predictor Host: {self.predictor_host}:{self.predictor_port}")
+    #################################### NETWORK THREAD #################################################################
+    def _run_worker(self): #build the asynch system 
+        asyncio.set_event_loop( self.loop ) # assign the event loop to the asynch working thread 
+        self.loop.run_until_complete( self.event_worker() ) # start the loop and pass the main corutine loop deamon task
+
+    async def event_worker(self): #allow to manage all the msg dropped into the queue by the corutine launch from the posprocess
+        pending_tasks = set()
+        # Opens a single, reusable pool of HTTP connections 
+        async with aiohttp.ClientSession() as session:
+            while True: #loop
+                # Non-blocking pause. Thread "sleeps" here using 0% CPU until a message drops into the queue
+                preds = await self.event_queue.get()
+                # corutine_finger_print=self._send_batch(session, preds)  -> crea the corutine
+                #task = asyncio.create_task (corutine_finger_print)-> schedule the corutine and creating a task7
+                # task is schedule into the event loop for a concorrent execution. The event loop engine accepts the task, puts it into its active calendar, and executes it concurrently whenever other tasks are paused or waiting for the network
+                task = asyncio.create_task( self._send_batch(session, preds) ) # do not wait for the response !!! fast fire-and-forget
+                pending_tasks.add(task)
+                task.add_done_callback(pending_tasks.discard) #clean up the back ground task state
+    # the original thread loop can yield back the control as soon as there is a network task wait
+    async def _send_batch(self, session: aiohttp.ClientSession, preds: List[Dict]): #asynch func, it is a corutine
+        #payload = {"instances": [p["embedding"] for p in preds]}
+        payload = {"instances": [p["embedding"][:4] for p in preds]} 
+        headers = {
+            "Host": self.broker_host,
+            "Ce-Id": str(uuid.uuid4()),
+            "Ce-Specversion": "1.0",
+            "Ce-Type": self.ce_type,
+            "Ce-Source": self.name,
+            "Content-Type": "application/json",
+        }
+        try:
+            #execute the asynch network call to kafka
+            async with session.post(self.broker, json=payload, headers=headers, timeout=3) as resp:
+                resp.raise_for_status()
+                logger.info(f"Kafka event sent (batch of {len(preds)})")
+        except Exception as e:
+            logger.error(f"Kafka error: {e}")
+        finally: #run in case of success or not
+            self.event_queue.task_done() #best practise
+    #######################################################################################################################
+    def preprocess(self, payload: Dict[str, Any], headers: Optional[Dict[str, str]] = None) -> Dict[str, List[List[float]]]:
         processed = []
         for inst in payload["instances"]:
             img = tf.image.decode_image(base64.b64decode(inst["image"]["b64"]), channels=3)
@@ -36,60 +93,45 @@ class ImageTransformer(Model):
             processed.append(img.numpy().tolist())
         return {"instances": processed}
 
-    def postprocess( self, outputs: Dict[str, Any], headers: Optional[Dict[str, str]] = None ) -> Dict[str, Any]: # in post processing parsing the inference model output 
-        pred = outputs["predictions"][0]
-        return { "predicted_class": int(pred["predicted_class"]), "probabilities": pred["probabilities"], "embedding": pred["embedding"], }
+    def postprocess(self, outputs: Dict[str, Any], headers: Optional[Dict[str, str]] = None) -> Dict[str, Any]:
+        preds = outputs["predictions"]
+        # self.event_queue.put(preds) is the coroutine object and the exact operation (the instruction to add data to the queue)
+        # It is executed entirely by the background working thread
+        #It uses self.loop as the engine to drive that execution
+        # it does the same operation as task = asyncio.create_task( self._send_batch(session, preds) )  BUT into the event engine loop of the working thread 
+        asyncio.run_coroutine_threadsafe( self.event_queue.put(preds), self.loop ) #bridge between two threads
+
+        pred = preds[0]
+        logger.info(f"POST PROCESS {self.count}")
+        self.count += 1
+        return {
+            "predicted_class": int(pred["predicted_class"]),
+            "probabilities": pred["probabilities"],
+            "embedding": pred["embedding"],
+        }
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--model_names", required=True, help="List of models and , as a splitter -> model1, model2, model3")
-    parser.add_argument("--namespace", default="default", help="K8s Namespace")
-    parser.add_argument("--predictor_port", type=int, default=8080) 
-    args, _ = parser.parse_known_args()
-    # model list name parsing base on ,
-    models_to_serve = [name.strip() for name in args.model_names.split(",") if name.strip()]
-    
-    if not models_to_serve:
-        raise ValueError("ERROR in the models name list param ")
 
-    # ImageTransformer for each inference CNN service 
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--model_names", required=True, help="List of models separated by commas -> model1,model2,model3")
+    parser.add_argument("--namespace", default="default", help="K8s Namespace")
+    parser.add_argument("--predictor_port", type=int, default=8080)
+    parser.add_argument("--broker", default="http://kafka-broker-ingress.knative-eventing.svc.cluster.local/default/kafka-broker")
+    parser.add_argument("--broker_host", default="kafka-broker-ingress.knative-eventing.svc.cluster.local")
+    parser.add_argument("--ce_type", default="org.kubeflow.serving.inference.request")
+
+    args, _ = parser.parse_known_args()
+
+    models_to_serve = [name.strip() for name in args.model_names.split(",") if name.strip()]
+    if not models_to_serve:
+        raise ValueError("ERROR: no model names provided")
+
     instances_list = []
     for model_name in models_to_serve:
-        transformer_instance = ImageTransformer( name=model_name, namespace=args.namespace,  predictor_port=args.predictor_port )
-        instances_list.append(transformer_instance)
+        transformer = ImageTransformer( name=model_name, namespace=args.namespace, predictor_port=args.predictor_port, broker=args.broker, broker_host=args.broker_host,ce_type=args.ce_type,)
+        instances_list.append(transformer)
 
-    logger.info(f"ModelServer started, {len(instances_list)} Transformer loaded")
-    #even if we use a list of model kserve create a trasformer pod for each one
-    ModelServer().start(instances_list) # we have to pass the entire list to ModelServer
-
-"""
-http://localhost:8080 incress point becasue the port forwarding
-header Host: simple-cnn.default.example.com ->used by istio gataway to route and rach the inference service, requesto to InferenceService simple-cnn in name space default
-
-gataway send the request to a trasformer before like simple-cnn-transformer.default.svc.cluster.local
-
-curl -X POST http://localhost:8080/v1/models/simple-cnn:predict \
-     -H "Host: simple-cnn.default.example.com" \
-     -H "Content-Type: application/json" \
-     -d @image.json
-##########################################################################
-### -> /v1/models/simple-cnn:predict it is the resource URI into server
-### -> simple-cnn.default.example.com the gataway that receive the post re router all to this service 
-kubectl port-forward --namespace istio-system svc/istio-ingressgateway 8080:80
-"""
-
-
-"""
-transformer:
-    containers:
-    - name: kserve-container
-      image: tomasconti02/image-transformer:v14   
-      command: ["python", "-m", "transformer"]
-      args:
-      - --model_names
-      - "simple-cnn,simple-cnn-test"               
-      - --namespace
-      - "default"                                
-
-"""
+    logger.info(f"ModelServer started, {len(instances_list)} Transformer(s) loaded")
+    ModelServer().start(instances_list)
