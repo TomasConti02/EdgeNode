94d71175bdf046cf663b501d21540bcb28c7f2bb
TomasConti02 <tomasconti34@gmail.com>
2026-07-23 12:03:34 +0200

    push the new stream tester

--- InferenceService/model_testing/stream_inference.sh
+++ InferenceService/model_testing/stream_inference.sh
@@ -3,33 +3,27 @@
 echo "Start."
 
 while true; do
-    echo "--- launch req to simple-cnn ---"
-    
-    #curl -s -X POST http://localhost:8080/v1/models/simple-cnn:predict \
-    curl -s -X POST http://172.18.255.200/v1/models/simple-cnn:predict \
-         -H "Host: simple-cnn.default.example.com" \
-         -H "Content-Type: application/json" \
-         -d @image.json > /dev/null
+
+    echo "--- launch batch request ---"
+
+    curl -sf -X POST \
+        "http://localhost:8080/predict_batch_encoded?models=simple-cnn,simple-cnn-test" \
+        -H "Host: image-api.default.example.com" \
+        -H "Content-Type: application/octet-stream" \
+        -H "X-Image-Sizes: 674,674" \
+        --data-binary @batch.bin \
+        > /dev/null
+
 
     if [ $? -eq 0 ]; then
-        echo "simple-cnn response received"
+        echo "Batch response received"
     else
-        echo "Error in simple-cnn response"
+        echo "Error in batch response"
     fi
 
-    echo -e "\n--- launch req to simple-cnn-test ---"
-    #curl -s -X POST http://localhost:8080/v1/models/simple-cnn-test:predict \
-    curl -s -X POST http://172.18.255.200/v1/models/simple-cnn-test:predict \
-         -H "Host: simple-cnn-test.default.example.com" \
-         -H "Content-Type: application/json" \
-         -d @image.json > /dev/null
 
-    if [ $? -eq 0 ]; then
-        echo "simple-cnn-test response received"
-    else
-        echo "Error in simple-cnn-test response"
-    fi
+    echo "..."
 
-    echo -e "\n..."
-    sleep 2
+    sleep 0.5
+
 done
