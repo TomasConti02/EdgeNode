a943c40d12bacc392357ebd0f0d5f4da98442fb7
TomasConti02 <tomasconti34@gmail.com>
2026-07-31 20:22:30 +0200

    light results

---
+++ InferenceService/model_testing/load_light.js
@@ -0,0 +1,67 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+import { Trend } from 'k6/metrics';
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+import { textSummary } from "https://jslib.k6.io/k6-summary/0.1.0/index.js";
+
+const inferenceDuration = new Trend('inference_processing_time');
+
+export const options = {
+  stages: [
+    { duration: '1m', target: 5 },
+    { duration: '2m', target: 15 },
+    { duration: '3m', target: 30 },
+    { duration: '3m', target: 30 },
+    { duration: '1m', target: 0 },
+  ],
+  thresholds: {
+    'http_req_failed': ['rate<0.01'],
+    'inference_processing_time': ['p(95)<4000', 'p(99)<5000'],
+  },
+};
+
+const imageData = open('./immagine.png', 'b');
+
+export function testModel(modelName) {
+  const port = modelName === 'simple-cnn-test' ? '30081' : '30080';
+  const url = `http://127.0.0.1:${port}/v1/models/${modelName}:predict`;
+  
+  const params = {
+    headers: {
+      'Content-Type': 'image/png',
+    },
+    timeout: '15s',
+  };
+
+  const res = http.post(url, imageData, params);
+  
+  const success = check(res, {
+    'status is 200': (r) => r.status === 200,
+    'has predicted_class': (r) => {
+      try {
+        const json = r.json();
+        return json && json.predicted_class !== undefined;
+      } catch (e) {
+        return false;
+      }
+    },
+  });
+
+  if (success) {
+    inferenceDuration.add(res.timings.duration);
+  }
+
+  sleep(0.2);
+}
+
+export default function() {
+  const modelName = __ITER % 2 === 0 ? 'simple-cnn' : 'simple-cnn-test';
+  testModel(modelName);
+}
+
+export function handleSummary(data) {
+  return {
+    "report-simple-cnn-test.html": htmlReport(data),
+    stdout: textSummary(data, { indent: " ", enableColors: true }),
+  };
+}
